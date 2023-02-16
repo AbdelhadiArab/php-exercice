30ea509e0eafca2337f80ecd77b6508db1364e84
abdelhadicodi <aboude.arab12@gmail.com>
2023-02-16 13:17:02 +0200

    basic-01,step-3

---
+++ basic-01/step_3.php
@@ -0,0 +1,12 @@
+<?php
+$name = "abdelhadi";
+$email = "abdelhadiarab01@gmail.com";
+echo "My name is $name and my email is $email\n";
+
+$height = 5;
+$width = 5;
+$area = $height * $width;
+
+//output
+echo "The area is $area\n" 
+?>
