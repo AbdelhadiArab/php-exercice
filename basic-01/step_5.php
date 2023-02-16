0fc6f5910bf11a018132d1cb0c8c2b277e1f6b57
abdelhadicodi <aboude.arab12@gmail.com>
2023-02-16 15:13:24 +0200

    basic-01,step-5

---
+++ basic-01/step_5.php
@@ -0,0 +1,14 @@
+<?php
+$course_name = "php";
+$enrolled_students = "20";
+$price = "29.99 $";
+$on_discount = "yes / no";
+
+echo "Course title: $course_name";
+echo '<br/>';
+echo "Enrolled students: $enrolled_students";
+echo '<br/>';
+echo "Course price: $price";
+echo '<br/>';
+echo "Course on discount: $on_discount"
+?>
