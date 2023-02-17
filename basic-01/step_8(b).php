6f7b07afa8ae2bec970cc92209632418684dab71
abdelhadicodi <aboude.arab12@gmail.com>
2023-02-17 13:41:53 +0200

    basic-01,step-8(b)

---
+++ basic-01/step_8(b).php
@@ -0,0 +1,8 @@
+<?php
+$multidimensional = array (
+    array("eggs",'balade','mazere3'),
+    array("milk",'fresh','taanayel'),
+    array("water-pack",'tanoureen','reem')
+);
+echo "Hey Sir, Please I need 1 pack of " .$multidimensional[0][1]. "\n" .$multidimensional[0][0]. "\n and 3\n".$multidimensional[2][2]. "\n" .$multidimensional[2][0];
+?>
