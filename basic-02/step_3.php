226ee197a4f383292050b4437ba0f2db676f7f18
abdelhadicodi <aboude.arab12@gmail.com>
2023-02-17 16:38:23 +0200

    basic-02,step-3

---
+++ basic-02/step_3.php
@@ -0,0 +1,12 @@
+<?php
+$var1 = 32;
+$var2 = 45;
+//before swap
+print "var1= $var1 and var2= $var2 <br/>";
+
+$swap = $var1;
+$var1 = $var2;
+$var2 = $swap;
+//after swap
+echo "var1= $var1 and var2= $var2";
+?>
